7a09af3b35fae99077498ccfc5c458642540dfe2
Gustavo Aguiar <gustavoaguiar14325@gmail.com>
2024-05-22 09:33:47 -0300

    commit2

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -39,7 +39,6 @@ export function Home() {
     }
 
     function createCard() {
-        event?.preventDefault()
         const cardName = document.getElementById('name').value
         const cardDate = document.getElementById('date').value
         let date
@@ -112,13 +111,8 @@ export function Home() {
     function orderRemindersByDate() {
         for (let i = 0; i < reminders.length - 1; i++) {
             for (let j = reminders.length - 1; j > i; j--) {
-                const date1 = (reminders[j - 1].reminderDate)
-                const date2 = (reminders[j].reminderDate)
                 
-                const diff = moment(date2,"DD/MM/YYYY").diff(moment(date1,"DD/MM/YYYY"))
-                console.log(diff);
-                
-                if (diff<=1) {
+                if (compareData(reminders[j - 1].reminderDate,reminders[j].reminderDate)) {
                     const temp = reminders[j]
                     reminders[j] = reminders[j - 1]
                     reminders[j - 1] = temp
@@ -127,6 +121,20 @@ export function Home() {
         }
 
         return true
+    }
+    
+    function compareData(date1:string,date2:string){
+        const diff = moment(date2,"DD/MM/YYYY").diff(moment(date1,"DD/MM/YYYY"))
+
+        let isSmaller
+
+        if(diff<=1){
+            isSmaller = true
+        }else{
+            isSmaller = false
+        }
+
+        return isSmaller
     }
 
     return (
